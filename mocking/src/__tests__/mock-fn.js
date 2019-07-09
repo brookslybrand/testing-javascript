83568ef75319ffa652ee253421a62c94266a9c57
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2019-07-09 07:48:12 -0500

    Finish mocking lesson

--- mocking/src/__tests__/mock-fn.js
+++ mocking/src/__tests__/mock-fn.js
@@ -1,26 +1,29 @@
-const assert = require('assert')
 const thumbWar = require('../thumb-war')
 const utils = require('../utils')
 
-function fn(impl) {
-  const mockFn = (...args) => {
-    mockFn.mock.calls.push(args)
-    return impl(...args)
-  }
-  mockFn.mock = {calls: []}
-  return mockFn
-}
-
 test('returns winner', () => {
   const originalGetWinner = utils.getWinner
-  utils.getWinner = fn((p1, p2) => p1)
+  utils.getWinner = jest.fn((p1, p2) => p1)
 
   const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
-  assert.strictEqual(winner, 'Kent C. Dodds')
-  assert.deepStrictEqual(utils.getWinner.mock.calls, [
+  expect(winner).toBe('Kent C. Dodds')
+  expect(utils.getWinner.mock.calls).toEqual([
     ['Kent C. Dodds', 'Ken Wheeler'],
-    ['Kent C. Dodds', 'Ken Wheeler'],
+    ['Kent C. Dodds', 'Ken Wheeler']
   ])
+  // could also do these assertions:
+  expect(utils.getWinner).toHaveBeenCalledTimes(2)
+  expect(utils.getWinner).toHaveBeenNthCalledWith(
+    1,
+    'Kent C. Dodds',
+    'Ken Wheeler'
+  )
+  expect(utils.getWinner).toHaveBeenNthCalledWith(
+    2,
+    'Kent C. Dodds',
+    'Ken Wheeler'
+  )
+
   // cleanup
   utils.getWinner = originalGetWinner
 })
