510d0719bc9998ac681f2dc5d720f6d55da5d84a
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2019-10-07 16:48:18 -0500

    Providers and stuff

--- jest-cypress/jest.config.js
+++ jest-cypress/jest.config.js
@@ -1,7 +1,16 @@
+const path = require('path')
+
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
+  moduleDirectories: [
+    'node_modules',
+    path.join(__dirname, 'src'),
+    'shared',
+    path.join(__dirname, 'test'),
+  ],
   moduleNameMapper: {
-    '\\.module\\.css': 'identity-obj-proxy',
+    '\\.module\\.css$': 'identity-obj-proxy',
     '\\.css$': require.resolve('./test/style-mock.js'),
   },
+  setupTestFrameworkScriptFile: require.resolve('./test/setup-tests.js'),
 }
